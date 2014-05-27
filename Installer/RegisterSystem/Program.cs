4ca5e50e588e41106ef5e9481fcbfbe9f5edbabf
Robin <r.tripp.1994@gmail.com>
2014-05-27 08:24:33 +0200

    More modular

--- Installer/RegisterSystem/Program.cs
+++ Installer/RegisterSystem/Program.cs
@@ -18,31 +18,76 @@ namespace RegisterSystem
         public static extern bool Wow64EnableWow64FsRedirection(ref IntPtr ptr);
 
         private static bool isBoxed = (!Environment.Is64BitProcess && Environment.Is64BitOperatingSystem);
-        public static readonly RegistryKey Software = RegistryKey.OpenBaseKey(Microsoft.Win32.RegistryHive.LocalMachine, (isBoxed) ? RegistryView.Registry64 : RegistryView.Default).OpenSubKey("SOFTWARE", true);
+        //public static readonly RegistryKey Software = RegistryKey.OpenBaseKey(Microsoft.Win32.RegistryHive.LocalMachine, (isBoxed) ? RegistryView.Registry64 : RegistryView.Default).OpenSubKey("SOFTWARE", true);
+
         static void Main(string[] args)
         {
             try
             {
-                //Console.WriteLine("Generating Registry Structure...");
-                RegistryKey Base = Software.CreateSubKey("NFC-Ring");
-                Base = Base.CreateSubKey("WinLogin");
+                Console.WriteLine("Generating Registry Structure...");
+                RegistryKey regKey = createRegistryStructure();
 
-                //Console.WriteLine("Adding default configuration...");
-                Base.SetValue("Port", "COM3");
-                Base.SetValue("Message Start", 0x02);
-                Base.SetValue("Message End", 0x0d);
+                Console.WriteLine("Adding default configuration...");
+                setRegitryBaseValues(regKey);
 
-                //Console.WriteLine("Reversing Entropy...");
+                Console.WriteLine("Reversing Entropy...");
                 string randomSalt = Hash(((new Random()).Next(int.MaxValue) + DateTime.Now.Ticks).ToString());
 
-                //Console.WriteLine("Enumerating the null set...");
-                Base = Base.CreateSubKey("Keys");
-                Base.SetValue("Salt", randomSalt);
+                Console.WriteLine("Enumerating the null set...");
+                regKey = regKey.CreateSubKey("Keys");
+                regKey.SetValue("Salt", randomSalt);
 
-                Console.WriteLine("Doing magical Windows-y things...");
-                string provider = (Environment.Is64BitOperatingSystem)? "64" : "32";
-                provider = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location) + "\\CredentialProviders\\" + provider + "\\NfcRingCredentialProvider.dll";
-                string to = Environment.GetEnvironmentVariable("SystemRoot") + "\\System32\\NfcRingCredentialProvider.dll";
+                Console.WriteLine("Register CredentialProvider in Windows...");
+                registerCredentialProviderDLL();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+                throw ex;
+            }
+        }
+
+        private static RegistryKey createRegistryStructure()
+        {
+            try
+            {
+                RegistryKey regKey = RegistryKey.OpenBaseKey(Microsoft.Win32.RegistryHive.LocalMachine, (isBoxed) ? RegistryView.Registry64 : RegistryView.Default).OpenSubKey("SOFTWARE", true);
+
+                regKey = regKey.CreateSubKey("NFC-Ring");
+                regKey = regKey.CreateSubKey("WinLogin");
+
+                return regKey;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+                throw ex;
+            }
+        }
+        private static void setRegitryBaseValues(RegistryKey regKey)
+        {
+            try
+            {
+                regKey.SetValue("Port", "COM2");
+                regKey.SetValue("Message Start", 0x02);
+                regKey.SetValue("Message End", 0x0d);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+                throw ex;
+            }
+        }
+        private static void registerCredentialProviderDLL()
+        {
+            try
+            {
+                string provider = (Environment.Is64BitOperatingSystem) ? "64" : "32";
+                string basePath = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location) + "\\CredentialProviders\\" + provider + "\\NfcRingCredentialProvider.dll";
+                string targetPath = Environment.GetEnvironmentVariable("SystemRoot") + "\\System32\\NfcRingCredentialProvider.dll";
 
                 IntPtr p = new IntPtr();
 
@@ -51,11 +96,11 @@ namespace RegisterSystem
                     Wow64DisableWow64FsRedirection(ref p);
                 }
 
-                if (File.Exists(to))
+                if (File.Exists(targetPath))
                 {
-                    File.Delete(to);
+                    File.Delete(targetPath);
                 }
-                File.Copy(provider, to);
+                File.Copy(basePath, targetPath);
 
                 if (isBoxed)
                 {
@@ -72,11 +117,11 @@ namespace RegisterSystem
 
         private static string Hash(string toHash)
         {
-            System.Security.Cryptography.SHA1CryptoServiceProvider x = new System.Security.Cryptography.SHA1CryptoServiceProvider();
+            System.Security.Cryptography.SHA1CryptoServiceProvider cryptProvider = new System.Security.Cryptography.SHA1CryptoServiceProvider();
             byte[] data = System.Text.Encoding.ASCII.GetBytes(toHash);
-            data = x.ComputeHash(data);
-            string o = BitConverter.ToString(data).Replace("-", "").ToUpper();
-            return o;
+            data = cryptProvider.ComputeHash(data);
+            string result = BitConverter.ToString(data).Replace("-", "").ToUpper();
+            return result;
         }
     }
 }
