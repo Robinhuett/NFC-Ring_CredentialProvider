e7835759d1643e09e7eb0c327fabc676458674a9
Robin <r.tripp.1994@gmail.com>
2014-05-28 14:58:32 +0200

    Cleaning

--- Installer/RegisterSystem/Program.cs
+++ Installer/RegisterSystem/Program.cs
@@ -3,10 +3,6 @@ using System;
 using System.IO;
 using System.Runtime.InteropServices;
 
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-
 namespace RegisterSystem
 {
     class Program
